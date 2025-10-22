005e55776783d9dc32e74ad4de8c8d373459f666
fabiane9258 <fabiane9258@gmail.com>
2025-10-22 15:28:12 +0300

    Enhance age calculation program with user prompts and age category output

--- practice/age.py
+++ practice/age.py
@@ -3,7 +3,7 @@ def main():
     CURRENT_YEAR = 2025
 
     while True:
-        user_input = input("Enter your Year of Birth: ").strip()
+        user_input = input("Enter your Year of Birth (or 'exit' to exit): ").strip()
         
         if user_input.lower() == "exit":
             print("I hope I was of help, Goodbye!")
@@ -12,12 +12,20 @@ def main():
         try:
             birth_year = int(user_input)
             
-            if birth_year < 1900  or birth_year > 2025:
+            if birth_year < 1900  or birth_year > CURRENT_YEAR:
                 print("Please enter a reasonable Year!")
                 continue
                 
             age = CURRENT_YEAR - birth_year
-            print(f"Since you were born in {birth_year}, you are {age} years old.")
+            
+            if age <= 12:
+                print(f"You are {age} years, you are  a Child")
+            elif 13 <= age <= 19:
+                print(f"You are {age} Years old, you are a Teenager")
+            elif 20 <= age <= 60:
+                print(f"You are {age} Years old, you are an Adult")
+            else:
+                print(f"You are {age} Years old, you are a Senior")
             
         except ValueError:
             print("Invalid Input, please enter correct format for Year of Birth!")
